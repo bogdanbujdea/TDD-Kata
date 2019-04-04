09e747f40070dff8daf33cdf9ac14c52100df7ac
Bogdan Bujdea <bogdan@thewindev.net>
2019-04-04 18:21:50 +0300

    Moved StringCalculator class to its own project

--- MyCalculator.UnitTests/CalculatorTests.cs
+++ MyCalculator.UnitTests/CalculatorTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Runtime.Serialization.Formatters;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -131,55 +129,6 @@ namespace MyCalculator.UnitTests
         private static StringCalculator CreateStringCalculator()
         {
             return new StringCalculator();
-        }
-    }
-
-    public class StringCalculator
-    {
-        public const int DefaultValue = 0;
-
-        public int Add(string numbers)
-        {
-            if (numbers.Contains("-"))
-            {
-                throw new ArgumentException("negatives not allowed");
-            }
-            if (numbers.StartsWith("//"))
-            {
-                numbers = ParseNumbersWithDelimiter(numbers);
-            }
-            if (numbers == "")
-            {
-                return DefaultValue;
-            }
-
-            return AddNumbers(numbers);
-        }
-
-        private string ParseNumbersWithDelimiter(string numbers)
-        {
-            var delimiter = GetDelimiter(numbers);
-            var numbersLine = GetNumbers(numbers);
-            numbersLine = numbersLine.Replace(delimiter, ',');
-            return numbersLine;
-        }
-
-        private char GetDelimiter(string numbers)
-        {
-            return numbers[2];
-        }
-
-        private string GetNumbers(string numbers)
-        {
-            return numbers.Substring(4);
-        }
-
-        private static int AddNumbers(string numbers, params char[] delimiters)
-        {
-            var defaultDelimiters = new[] { ',', '\n' };
-            return numbers.Split(defaultDelimiters.Concat(delimiters).ToArray())
-                .Select(int.Parse)
-                .Sum();
         }
     }
 }
---
+++ MyCalculator/StringCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace MyCalculator
+{
+    public class StringCalculator
+    {
+        public const int DefaultValue = 0;
+
+        public int Add(string numbers)
+        {
+            if (numbers.Contains("-"))
+            {
+                throw new ArgumentException("negatives not allowed");
+            }
+            if (numbers.StartsWith("//"))
+            {
+                numbers = ParseNumbersWithDelimiter(numbers);
+            }
+            if (numbers == "")
+            {
+                return DefaultValue;
+            }
+
+            return AddNumbers(numbers);
+        }
+
+        private string ParseNumbersWithDelimiter(string numbers)
+        {
+            var delimiter = GetDelimiter(numbers);
+            var numbersLine = GetNumbers(numbers);
+            numbersLine = numbersLine.Replace(delimiter, ',');
+            return numbersLine;
+        }
+
+        private char GetDelimiter(string numbers)
+        {
+            return numbers[2];
+        }
+
+        private string GetNumbers(string numbers)
+        {
+            return numbers.Substring(4);
+        }
+
+        private static int AddNumbers(string numbers, params char[] delimiters)
+        {
+            var defaultDelimiters = new[] { ',', '\n' };
+            return numbers.Split(defaultDelimiters.Concat(delimiters).ToArray())
+                .Select(int.Parse)
+                .Sum();
+        }
+    }
+}
